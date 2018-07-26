b6f24fdc891c4884dd26a670d7d7e3115be4f76c
Stevan Popovic <stevanpopovic5@gmail.com>
2018-07-26 11:29:30 +0100

    Setting up files for new wordpress theme

---
+++ front-page.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>stevanpopovic.com</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
+    <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js"></script>
+  </head>
+  <body>
+    <div class="container">
+      <section>
+        <h1>Stevan Popovic'</h1>
+        <h2>A commercially-minded software developer.</h2>
+      </section>
+      <div class="columns">
+        <div class="column is-one-half-desktop">
+          <h3>Currently</h3>
+          <ul>
+            <li>Looking for new opportunities</li>
+            <li>Building X</li>
+          </ul>
+
+          <h3>Previously</h3>
+          <ul>
+            <li>Business Development &amp; Partnerships at Deliveroo</li>
+            <li>Partnership Develpoment at YPlan</li>
+          </ul>
+
+          <h3>Writing</h3>
+          <ul>
+            <li>Post One</li>
+            <li>Post Two</li>
+          </ul>
+        </div>
+        <div class="">
+
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
