c4bd73d872e7354657f75da32fa0957322bde0b5
Stevan Popovic <stevanpopovic5@gmail.com>
2018-08-03 21:31:47 +0100

    Sorting column layour on the archives page

--- archives.php
+++ archives.php
@@ -6,14 +6,14 @@ get_header(); ?>
 
 	<div class="post">
 
-		<!--
 
-		<?php the_post(); ?>
+
+		<!-- <?php the_post(); ?>
 		<h2 class="entry-title"><?php the_title(); ?></h2>
 
-		<?php get_search_form(); ?>
+		<?php get_search_form(); ?> -->
 
-		<h4>Archives by Month:</h4>
+		<!-- <h4>Archives by Month:</h4>
 		<ul>
 			<?php wp_get_archives('type=monthly'); ?>
 		</ul>
@@ -21,46 +21,30 @@ get_header(); ?>
 		<h4>Archives by Subject:</h4>
 		<ul>
 			 <?php wp_list_categories(); ?>
-		</ul>
-
-		-->
+		</ul> -->
 
 		<h2>All Posts</h2>
 
-		<?php query_posts('showposts=50'); if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+      <div class="columns">
+        <div class="column is-10">
+          <div class="columns is-multiline">
+            <?php query_posts('showposts=20'); if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+              <article class="columns column is-12 archive-block">
+                <div class="column is-3">
+                  <h6><?php the_tags('',', ' ); ?></h6>
+                  <small><?php the_time('F jS, Y'); ?></small>
+                </div>
+                <div class="column is-9">
+                  <h2 class="archive-page"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Go to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+                </div>
+              </article>
+            <?php endwhile; else : ?>
+            <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+            <?php endif; ?>
+          </div>
 
-		 	<!-- Display the Title as a link to the Post's permalink. -->
-
-		 	<h3><a href="<?php the_permalink(); ?>" rel="bookmark" title="Go to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h3>
-
-		 	<!-- Display the date (November 16th, 2009 format) and a link to other posts by this posts author. -->
-
-		 	<small><?php the_time('F jS, Y'); ?><!-- by <?php the_author_posts_link(); ?>--></small>
-
-
-		 	<!-- Display the Post's content in a div box.
-		 	<div class="entry">
-		 		<?php the_content(); ?>
-		 	</div> -->
-
-
-		 	<!-- Display a comma separated list of the Post's Categories.
-		 	<p class="postmetadata"><?php _e( 'Posted in' ); ?> <?php the_category( ', ' ); ?></p>
-		    -->
-
-		 	<!-- Stop The Loop (but note the "else:" - see next line). -->
-
-		 <?php endwhile; else : ?>
-
-
-		 	<!-- The very first "if" tested to see if there were any Posts to -->
-		 	<!-- display.  This "else" part tells what do if there weren't any. -->
-		 	<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-
-
-		 	<!-- REALLY stop The Loop. -->
-		 <?php endif; ?>
-
+        </div>
+      </div>
 
 	</div><!-- #post -->
 
