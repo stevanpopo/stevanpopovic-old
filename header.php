34a20e6fd9ea0f4429eeb2ad50d36d5e609f63f0
Stevan Popovic <stevanpopovic5@gmail.com>
2018-08-03 13:50:23 +0100

    Created basic page.php

--- header.php
+++ header.php
@@ -14,9 +14,9 @@
       <header>
         <nav class="primary-nav">
           <ul>
-            <li><a href="#">Articles</a> &middot;</li>
-            <li><a href="#">About</a> &middot;</li>
-            <li><a href="#">Projects</a></li>
+            <li><a href="./writing">Articles</a> &middot;</li>
+            <li><a href="./about">About</a> &middot;</li>
+            <li><a href="./projects">Projects</a></li>
           </ul>
         </nav>
       </header>
---
+++ page.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template Name: Page
+*/
+ get_header(); ?>
+
+	<div class='post'>
+		<?php if (have_posts()) : while (have_posts()) : the_post();?>
+
+			<h2><?php the_title(); ?></h2>
+
+			<?php the_content(); ?>
+        <?php endwhile; endif; ?>
+	</div>
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
