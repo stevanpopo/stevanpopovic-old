2a10d7f1f2ef9ead4afbed2d1d2b471221a6eb3a
Stevan Popovic <stevanpopovic5@gmail.com>
2018-08-06 12:12:02 +0100

    Vertically centered images in projects page. Added hr's and styling to project page

--- page.php
+++ page.php
@@ -4,7 +4,7 @@ Template Name: Page
 */
  get_header(); ?>
   <div class="columns">
-    <div class='post column is-8'>
+    <div class='post column is-10'>
   		<?php if (have_posts()) : while (have_posts()) : the_post();?>
 
   			<h2><?php the_title(); ?></h2>
@@ -12,7 +12,7 @@ Template Name: Page
   			<?php the_content(); ?>
           <?php endwhile; endif; ?>
   	</div>
-    <div class="column is-4">
+    <div class="column is-2">
 
     </div>
   </div>
