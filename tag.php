5a4ab2e0f57f04bba7c4a2da64a6fbb9609e0029
Stevan Popovic <stevanpopovic5@gmail.com>
2018-08-22 10:40:36 +0100

    Fixed the issue with misaligned headers

--- tag.php
+++ tag.php
@@ -5,34 +5,28 @@
 
 get_header(); ?>
 
-	<section id="primary" class="site-content">
-		<div id="content" role="main">
-      <div class="post">
-        <div class="columns">
-          <div class="column is-10-desktop">
-            <div class="columns is-multiline">
-              <?php if ( have_posts() ) : ?>
-
-                <h1>Tag: <?php single_tag_title(); ?></h1>
-
-              <?php while ( have_posts() ) : the_post(); ?>
-                <article class="columns column is-12 archive-block">
-                  <div class="post-meta-archive column is-one-fifth-desktop is-one-quarter-tablet is-four-quarters-mobile">
-                    <h6><?php the_tags('',', ' ); ?></h6>
-                    <small><?php the_time('F jS, Y'); ?></small>
-                  </div>
-                  <div class="column is-four-fifths-desktop is-three-quarters-tablet is-four-quarters-mobile">
-                    <h2 class="tag-page"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Go to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-                  </div>
-                </article>
-              <?php endwhile; else : ?>
-              <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
-              <?php endif; ?>
-            </div>
-          </div>
+  <div class="post">
+    <h2>Tag: <?php single_tag_title(); ?></h2>
+    <!-- <p>Here you can see the latest articles I have written on the topic of <?php single_tag_title(); ?>.</p> -->
+    <div class="columns">
+      <div class="column is-10-desktop">
+        <div class="columns is-multiline">
+          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+            <article class="columns column is-12 archive-block">
+              <div class="post-meta-archive column is-one-fifth-desktop is-one-quarter-tablet is-four-quarters-mobile">
+                <h6><?php the_tags('',', ' ); ?></h6>
+                <small><?php the_time('F jS, Y'); ?></small>
+              </div>
+              <div class="column is-four-fifths-desktop is-three-quarters-tablet is-four-quarters-mobile">
+                <h2 class="archive-page"><a href="<?php the_permalink(); ?>" rel="bookmark" title="Go to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+              </div>
+            </article>
+          <?php endwhile; else : ?>
+          <p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+          <?php endif; ?>
         </div>
       </div>
-		</div><!-- #content -->
-	</section><!-- #primary -->
+    </div>
+  </div>
 
 <?php get_footer(); ?>
