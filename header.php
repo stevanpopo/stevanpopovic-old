b8b180e0a0efa5c0a3864b83ecce6b6d080adb1d
Stevan Popovic <stevanpopovic5@gmail.com>
2018-08-29 11:21:34 +0100

    Changed main paragraph font to a serif

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>stevanpopovic.com</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.1/css/bulma.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,900|Muli:400,400i,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,900|Muli:400,400i,700|PT+Serif:400,400i,700" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
     <script defer src="https://use.fontawesome.com/releases/v5.1.0/js/all.js"></script>
   </head>
