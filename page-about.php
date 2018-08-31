65fa93ff2cb76f5e0f9cf106b74c92f8bbb1bf2b
Stevan Popovic <stevanpopovic5@gmail.com>
2018-08-31 17:41:10 +0100

    Added content on the site update to README

--- page-about.php
+++ page-about.php
@@ -7,7 +7,7 @@ Template Name: Page
     <div class='post column is-8'>
   		<?php if (have_posts()) : while (have_posts()) : the_post();?>
 
-  			<h1><?php the_title(); ?></h1>
+  			<h2><?php the_title(); ?></h2>
 
   			<?php the_content(); ?>
           <?php endwhile; endif; ?>
--- page-projects.php
+++ page-projects.php
@@ -7,7 +7,7 @@ Template Name: Projects Page
     <div class='post column is-10'>
   		<?php if (have_posts()) : while (have_posts()) : the_post();?>
 
-  			<h1><?php the_title(); ?></h1>
+  			<h2><?php the_title(); ?></h2>
 
   			<?php the_content(); ?>
           <?php endwhile; endif; ?>
